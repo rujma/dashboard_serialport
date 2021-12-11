c9f1d2e3c20cfb4e9ec42b9ec436e775edca9fbd
Rui Almeida <rjmalmeida0@gmail.com>
2021-12-11 22:17:52 +0000

    added string tokenization

--- cserialport.cpp
+++ cserialport.cpp
@@ -4,17 +4,23 @@
 #include "cserialport.h"
 
 
+/*
+ * @brief CSerialPort constructor
+ *
+ * @param {QString} comName Serial port identifier
+ * @param {QObject *} parent Parent QT object
+*/
 CSerialPort::CSerialPort(QString comName, QObject *parent) : QThread(parent)
 {
     m_quit = false;
     m_serial = new QSerialPort();
-    //const auto infos = QSerialPortInfo::availablePorts();
-    //for (const QSerialPortInfo &info : infos) {
-    //    qDebug() << info.portName();
-    //}
     startSerial(comName);
 }
 
+/*
+ * @brief CSerialPort destructor
+ *
+*/
 CSerialPort::~CSerialPort()
 {
     m_quit = true;
@@ -22,6 +28,10 @@ CSerialPort::~CSerialPort()
     delete m_serial;
 }
 
+/*
+ * @brief CSerialPort thread that processes serial port data
+ *
+*/
 void CSerialPort::run()
 {
     if(!m_serial->open(QIODevice::ReadWrite))
@@ -41,17 +51,35 @@ void CSerialPort::run()
             QByteArray serialData = m_serial->readAll();
             while (m_serial->waitForReadyRead(10))
                 serialData += m_serial->readAll();
-            /* Parse commands */
-            qDebug() << serialData;
+            /* Parse commands. Let's specify them:
+             *  <CMD> <VALUE>\r\n
+             *  Possible CMD:
+             *  kph <0...1024>\r\n
+             *  blk <0,1,2>\r\n
+             *  gas <0...1024>\r\n
+             *  tmp <0...1024>\r\n
+            */
+            QString serialDataStr = QString(serialData);
+            QStringList tokens = serialDataStr.trimmed().split(" ");
+            qDebug() << serialDataStr;
         }
     }
 
 }
+
+/*
+ * @brief CSerialPort function that stops and closes the serial port
+ *
+*/
 void CSerialPort::stop()
 {
-
+    m_quit = true;
 }
 
+/*
+ * @brief CSerialPort function that configures and starts the serial port
+ *
+*/
 void CSerialPort::startSerial(const QString &portName)
 {
     /* Configure the serial port */
